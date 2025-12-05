66664e43110273f118da1360ac880942294aff1e
HaiqalHarona <haronahaiqal@gmail.com>
2025-12-06 00:05:47 +0800

    Update playlistservice.js

--- service/playlistservice.js
+++ service/playlistservice.js
@@ -3,5 +3,39 @@ const playlist = require('../models/playlist.js');
 const user = require('../models/user.js');
 
 let playlistService = {
-    
+    createPlaylist: async function(playlistData) {
+        try {
+            const newPlaylist = await playlist.create(playlistData);
+            return newPlaylist;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getPlaylistsByUserId: async function(userId) {
+        try {
+            const foundPlaylists = await playlist.find({ user: userId });
+            return foundPlaylists;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    updatePlaylist: async function(playlistId, playlistData) {
+        try {
+            const updatedPlaylist = await playlist.findByIdAndUpdate(playlistId, playlistData, { new: true });
+            return updatedPlaylist;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    deletePlaylist: async function(playlistId) {
+        try {
+            const deletedPlaylist = await playlist.findByIdAndDelete(playlistId);
+            return deletedPlaylist;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
