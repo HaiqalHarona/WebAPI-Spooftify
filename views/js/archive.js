8ccb20cab8a8a18a58c16ad66258ec2decaf40bc
HaiqalHarona <haronahaiqal@gmail.com>
2025-12-24 14:15:43 +0800

    s

---
+++ views/js/archive.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    console.log("Archive page loaded");
+    loadArchivedPlaylists();
+});
+
+function loadArchivedPlaylists() {
+    const container = $('#archive-container');
+
+}
